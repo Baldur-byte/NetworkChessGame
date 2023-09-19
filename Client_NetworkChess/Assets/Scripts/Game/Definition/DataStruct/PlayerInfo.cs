c71ec36c80d0619dbed530936ba57d10b214f790
李东升 <lidongsheng>
2023-09-19 14:16:23 +0800

    更新协议

---
+++ Client_NetworkChess/Assets/Scripts/Game/Definition/DataStruct/PlayerInfo.cs
@@ -0,0 +1,17 @@
+﻿//@LeeTools
+//------------------------
+//Filename：PlayerInfo.cs
+//Auther：auus
+//Device：DESKTOP-DFRI604
+//Email：346679447@qq.com
+//CreateDate：2023/09/18 10:46:05
+//Function：Nothing
+//------------------------
+
+namespace ChessGame
+{
+    public class PlayerInfo
+    {
+        
+    }
+}
